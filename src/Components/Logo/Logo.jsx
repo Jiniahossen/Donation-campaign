728f65b53b38391e8e2823f7db10ede826a50faa
jinia hossen <jiniahossen31@gmail.com>
2023-09-27 21:29:03 +0600

    some changes for live deploy

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,7 +1,9 @@
+import logo from '../../assets/Resources/Logo.png'
+
 const Logo = () => {
     return (
         <div className=" w-44">
-            <img src="/public/Resources/Logo.png" alt="" className="w-44" />
+            <img src={logo} alt="" className="w-44" />
             
         </div>
     );
